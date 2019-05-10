429851ea32fb561f38ad1584721823feabb69662
hwu30 <hwu30@18MB-BIOS009.attlocal.net>
2019-05-09 22:36:41 -0400

    Modify computing pages

--- computing/CfromR/matmult.c
+++ computing/CfromR/matmult.c
@@ -13,7 +13,7 @@ SEXP matmult(SEXP X, SEXP Y) {
 	nrowX = INTEGER(dims)[0];
 	ncolX = INTEGER(dims)[1];
 	dims = getAttrib( Y, R_DimSymbol ) ;
-    ncolY = INTEGER(dims)[1];
+	ncolY = INTEGER(dims)[1];
 
 	/* allocate memory for the result matrix */
 	SEXP result = PROTECT( allocVector(REALSXP, nrowX*ncolY) );
@@ -24,18 +24,18 @@ SEXP matmult(SEXP X, SEXP Y) {
 	result_ptr = REAL(result);
 
 	/* call dgemm function to multiple two matrices */
-    double one = 1.0;
-    double zero = 0.0;
+	double one = 1.0;
+	double zero = 0.0;
 	F77_CALL(dgemm)("n", "n", &nrowX, &ncolY, &ncolX, &one, x_ptr, &nrowX, y_ptr, &ncolX,
-                    &zero, result_ptr, &nrowX);
+					&zero, result_ptr, &nrowX);
 	
 	/* manipulate the output to make it a matrix */
 	dims = PROTECT( allocVector(INTSXP, 2) );
 	INTEGER(dims)[0] = nrowX;
-    INTEGER(dims)[1] = ncolY;
+	INTEGER(dims)[1] = ncolY;
 	setAttrib( result, R_DimSymbol, dims ) ;
 	
-    UNPROTECT(2);
+	UNPROTECT(2);
 	return(result);
 }
 
