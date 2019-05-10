8a10293766b9c6e5bdc235124286f7cab4d25ad1
hwu30 <hwu30@18MB-BIOS009.attlocal.net>
2019-05-09 22:31:39 -0400

    Modify computing pages

--- computing/CfromR/matmult.c
+++ computing/CfromR/matmult.c
@@ -17,7 +17,7 @@ SEXP matmult(SEXP X, SEXP Y) {
 
 	/* allocate memory for the result matrix */
 	SEXP result = PROTECT( allocVector(REALSXP, nrowX*ncolY) );
-
+	
 	/* get pointers for the input/output */
 	x_ptr = REAL(X); 
 	y_ptr = REAL(Y);
@@ -28,13 +28,13 @@ SEXP matmult(SEXP X, SEXP Y) {
     double zero = 0.0;
 	F77_CALL(dgemm)("n", "n", &nrowX, &ncolY, &ncolX, &one, x_ptr, &nrowX, y_ptr, &ncolX,
                     &zero, result_ptr, &nrowX);
-
+	
 	/* manipulate the output to make it a matrix */
 	dims = PROTECT( allocVector(INTSXP, 2) );
 	INTEGER(dims)[0] = nrowX;
     INTEGER(dims)[1] = ncolY;
 	setAttrib( result, R_DimSymbol, dims ) ;
-
+	
     UNPROTECT(2);
 	return(result);
 }
