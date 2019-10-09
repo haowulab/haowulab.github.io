30a2158b4a1144b6fd041ee804d528f5be5c82db
hwu30 <hwu30@18MB-BIOS009.local>
2019-10-09 09:50:20 -0400

    update bios555 lab4

--- teaching/bioc/sgseq-lab.R
+++ teaching/bioc/sgseq-lab.R
@@ -1,39 +1,57 @@
-#### example scripts to play with BAM files.
+#### example scripts for BIOS555 lab3, updated 10/9/2019
+
+##############################################
+## using Rsubread for sequence alignment
+##############################################
+library(Rsubread)
+## build reference
+buildindex(basename="phage",reference="phage-ref.fa")
+## alignment
+align.stat <- align(index="phage",readfile1="phage-reads.fa",
+                    output_file="phage-reads.bam",type='dna')
+align.stat  # check the mapping results
+
+
+##############################################
+## Joint analysis for RNA- and ChIP-seq
+##############################################
 library(GenomicRanges)
 library(GenomicFeatures)
 library(Rsamtools)
 
 ## first get gene information from UCSC. This will take 2 minutes or so.
-## Or if you have these saved you can load them in using: gene.hg18=loadDb("gene.hg18.GR.sqlite")
-gene.hg18=makeTxDbFromUCSC(genom="hg18",tablename="knownGene")
-allgenes=genes(gene.hg18)
-allgenes.chr22=allgenes[seqnames(allgenes)=="chr22"] ## genes on  chr 22
+## Or you can download knowngene.hg18.sqlite from class site and do
+## gene.hg18=loadDb("knowngene.hg18.sqlite")
+
+gene.hg18 = makeTxDbFromUCSC(genom="hg18",tablename="knownGene")
+allgenes = genes(gene.hg18)
+allgenes.chr22 = allgenes[seqnames(allgenes)=="chr22"] ## genes on  chr 22
 
 ## Read in RNA-seq bam file
-what=c("rname", "strand", "pos", "qwidth")
-TSS.counts=NULL
-param=ScanBamParam(what = what)
-bam=scanBam("K562_RNAseq_chr22.bam", param=param)[[1]]
-IRange.reads=GRanges(seqnames=Rle(bam$rname),
-                     ranges=IRanges(bam$pos, width=bam$qwidth))
+what = c("rname", "strand", "pos", "qwidth")
+TSS.counts = NULL
+param = ScanBamParam(what = what)
+bam = scanBam("K562_RNAseq_chr22.bam", param=param)[[1]]
+IRange.reads = GRanges(seqnames=Rle(bam$rname),
+                       ranges=IRanges(bam$pos, width=bam$qwidth))
 
 ## get RNA-seq counts within gene bodies -
 ## should obtain exon counts and sum up. I'm approximating the process here.
-counts.RNAseq=countOverlaps(allgenes.chr22, IRange.reads)
+counts.RNAseq = countOverlaps(allgenes.chr22, IRange.reads)
 
 ## get Cmyc binding around TSS
 ## first get region around TSS (+/- 1000 bp). This is a little tricky, be careful in strands directions.
-strands=strand(allgenes.chr22)
-ix.gene.minus=which(strands=="-")
-TSS=start(allgenes.chr22)
-TSS[ix.gene.minus]=end(allgenes.chr22)[ix.gene.minus]
-ext=1000
-TSS.GRanges=GRanges(seqnames=Rle("chr22", length(allgenes.chr22)),
-                    ranges=IRanges(start=TSS-ext, end=TSS+ext))
-bam=scanBam("K562Cmyc_chr22.bam", param=param)[[1]]
-IRange.reads=GRanges(seqnames=Rle(bam$rname),
-                     ranges=IRanges(bam$pos, width=bam$qwidth))
-counts.Cmyc=countOverlaps(TSS.GRanges, IRange.reads)
+strands = strand(allgenes.chr22)
+ix.gene.minus = which(strands=="-")
+TSS = start(allgenes.chr22)
+TSS[ix.gene.minus] = end(allgenes.chr22)[ix.gene.minus]
+ext = 1000
+TSS.GRanges = GRanges(seqnames=Rle("chr22", length(allgenes.chr22)),
+                      ranges=IRanges(start=TSS-ext, end=TSS+ext))
+bam = scanBam("K562Cmyc_chr22.bam", param=param)[[1]]
+IRange.reads = GRanges(seqnames=Rle(bam$rname),
+                       ranges=IRanges(bam$pos, width=bam$qwidth))
+counts.Cmyc = countOverlaps(TSS.GRanges, IRange.reads)
 
 ## see if there's any correlation of gene expression and Cmyc binding.
 cor(log(counts.Cmyc+1), log(counts.RNAseq+1)) ## pretty good correlation in log scale
@@ -42,36 +60,34 @@ plot(counts.Cmyc, counts.RNAseq, log="xy", xlab="Cmyc binding", ylab="Gene expre
 ## Another practice: get window counts for Cmyc binding: counts in 50 bp windows around TSS
 ## This can show the peak shapes at binding sites
 ## take TSS +/- 1000 bp and cut into 50bp windows
-ext=1000
-winsize=50
-allss=sapply(TSS, function(x) seq(x-ext, x+ext-1, by=winsize))
+ext = 1000
+winsize = 50
+allss = sapply(TSS, function(x) seq(x-ext, x+ext-1, by=winsize))
 ## create GRanges
-allchrs=as.character(seqnames(allgenes.chr22))
-allseq=Rle(allchrs,rep(ext/winsize*2,length(allchrs)))
-allrange=IRanges(start=allss, end=allss+winsize-1)
-TSS.GRanges=GRanges(seqnames=allseq, ranges=allrange)
-tmp=countOverlaps(TSS.GRanges, IRange.reads)
-counts.Cmyc.win=matrix(tmp, ncol=40, byrow=TRUE)
+allchrs = as.character(seqnames(allgenes.chr22))
+allseq = Rle(allchrs,rep(ext/winsize*2,length(allchrs)))
+allrange = IRanges(start=allss, end=allss+winsize-1)
+TSS.GRanges = GRanges(seqnames=allseq, ranges=allrange)
+tmp = countOverlaps(TSS.GRanges, IRange.reads)
+counts.Cmyc.win = matrix(tmp, ncol=40, byrow=TRUE)
 plot(colMeans(counts.Cmyc.win), type="b", xaxt="n", xlab="position", ylab="Average Cmyc binding")
 axis(1, c(1, 10,20, 30,40), labels=c("-500", "-200", "TSS", "250", "500"))
 
 ####################################################################
-## part II. DE test for RNA-seq data
+## DE test for RNA-seq data
 ####################################################################
-library(DESeq)
+library(DESeq2)
 library(edgeR)
 load("X.rda")
 nreps=ncol(X)/2
 
-##  DEseq
-library(DESeq)
-conds=c(rep(0,nreps),rep(1,nreps))
-cds <- newCountDataSet(X, conds )
-cds <- estimateSizeFactors( cds )
-cds <- estimateDispersions( cds )
-fit.DEseq <- nbinomTest( cds, "0","1")
-pval.DEseq=fit.DEseq$pval
-pval.DEseq[is.na(pval.DEseq)]=1
+## DESeq2
+library(DESeq2)
+condition <- as.factor(c(rep(0,nreps),rep(1,nreps)))
+dds <- DESeqDataSetFromMatrix(X, DataFrame(condition), ~ condition)
+dds <- DESeq(dds)
+res <- results(dds)
+pval.DEseq <- res$pvalue
 
 ## edgeR
 library(edgeR)
